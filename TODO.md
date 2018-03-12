f02bcf451fda2cdc467b7cad9d0497a68999ccc4
Simon Ldj <simon.ldj@gmail.com>
2018-03-12 19:03:06 +0200

    Update logging method, add few examples
    Update logging method to send Hypnolog valid object according to new
    API.
    Add few logging examples.

---
+++ TODO.md
@@ -0,0 +1,9 @@
+HypnoLog-nodejs TODO list
+===================================
+
+- define some easy-to-use basic logging methods
+- make it as a package/library
+- handle when server is down
+- push some code to github
+- solve async issues (?) (logged data might appear in incorrect order)
+
--- client.js
+++ client.js
@@ -1,28 +1,23 @@
 var http = require('http');
 
-// TODO: warp all VDebug logger in some module (or class)
+// TODO: warp all HypnoLog logger in some module (or class)
+// TODO: handle when server is down
 
-function log(data) {
-
-    // TODO: support logging a string and warp it with an vdebug-log valid object
-
-    // if logged data not an object, create an error object instead
-    if (typeof data !== "object") {
-        console.error("Logged data expected to be an Object");
-        data = {
-            type : "vdebug-error",
-            error: "Vdebug error in Node.js client: Logged data expected to be an Object.",
-            loggedData : data,
-        }
+function log(data, type = "object") {
+    
+    // construct HypnoLog object according to the API
+    let json = {
+        data : data,
+        type : type
     }
 
-    post_data = JSON.stringify(data);
+    post_data = JSON.stringify(json);
 
     // An object of options to indicate where to post to
     var post_options = {
         host: 'localhost',
         port: '7000',
-        path: '/vdebug/in',
+        path: '/logger/in',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -46,18 +41,57 @@ function log(data) {
 
 // Usage Example:
 
-var obj = {
-    'value' : "Test post from Node.js client",
-    'number' : 55,
-    comObj: {
-        foo : "Fofo",
-        x   : 20
-    }
-};
+// log a string
+let str = "Hello HypnoLog from JavaScript!";
+log(str, "string");
 
-console.log("sending:");
-console.dir(obj);
+// log numbers array
+let numbers =  Array.from({length: 50}, (v, i) => (i*10)%100 + Math.floor(Math.random() * 10));
+log("Example of logging Array of numbers:", "string");
+log(numbers, "plot");
 
-// Log data
-log(obj);
+// TODO: Multi line graph
+
+// TODO: log 2d array
+//let array2d = Array.from({length: 10}, () => Array.from({length: 10}, () => Math.floor(Math.random() * 40)));
+//// TODO: call it 2d heat map
+//log(array2d, "2dnumbersarray");
+
+// TODO: histogram
+
+// log Lat-Long Geo locations using Google maps
+let locations = [
+    ['Lat', 'Long', 'Name'],
+    [37.4232, -122.0853, 'Work'],
+    [37.4289, -122.1697, 'University'],
+    [37.6153, -122.3900, 'Airport'],
+    [37.4422, -122.1731, 'Shopping']
+];
+log("Example of logging Lat-Long Geo locations using Google maps:", "string");
+log(locations, "GoogleMaps");
+
+// log custom object
+let rectangle = {
+    higth : 20,
+    width : 10,
+    color : "green"
+}
+log("Example of logging custom object:", "string");
+// note, default logging type is "object"
+log(rectangle);
+
+// log custom object with nested custom objects
+let car = {
+    brand : "Seat",
+    model : "Mii",
+    engine : {
+        numberOfCylinders : 3,
+        acceleration : 14.4
+        },
+        color : "red"
+    };
+log("Example of logging custom object with nested custom objects:", "string");
+log(car);
+
+// TODO: Example of logging with tags
 
