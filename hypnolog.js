ba18156a57791762c9d24b0507974eb4d285bcea
Simon Ldj <simon.ldj@gmail.com>
2018-03-19 20:11:18 +0200

    Handle better with server errors
    print them to the user's console

--- hypnolog.js
+++ hypnolog.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 // TODO: warp all HypnoLog logger in some module (or class)
 // TODO: handle when server is down
+// TODO: document according to node/javascript style
 
 function log(data, type = "object") {
     
@@ -11,6 +12,7 @@ function log(data, type = "object") {
         type : type
     }
 
+    // convert to JSON string
     post_data = JSON.stringify(json);
 
     // An object of options to indicate where to post to
@@ -27,10 +29,18 @@ function log(data, type = "object") {
 
     // Set up the request
     var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-        });
+        const { statusCode } = res;
+        if (statusCode !== 200) {
+            let error = new Error(`HypnoLog error: Request Failed, Status Code: ${statusCode}`);
+            console.error(error.message);
+        }
+        //res.setEncoding('utf8');
+        //res.on('data', function (chunk) {
+            //// print full response body
+            //console.log('Response: ' + chunk);
+        //});
+    }).on('error', (e) => {
+        console.error(`HypnoLog error: problem with request: ${e.message}`);
     });
 
     // post the data
