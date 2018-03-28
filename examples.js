e1a49e310c9fffb62bfa17a07dc616033a204f14
Simon Ldj <simon.ldj@gmail.com>
2018-03-29 02:55:52 +0300

    Add 2d array heatmap example
    Also, write "s" instead of "string" when logging string

--- examples.js
+++ examples.js
@@ -6,19 +6,25 @@ var HL = require('./hypnolog');
 
 // log a string
 let str = "Hello HypnoLog from JavaScript!";
-HL.log(str, "string");
+HL.log(str, "string"); // hint: use 's' instead of 'string'
 
 // log numbers array
 let numbers =  Array.from({length: 50}, (v, i) => (i*10)%100 + Math.floor(Math.random() * 10));
-HL.log("Example of logging Array of numbers:", "string");
+HL.log("Example of logging Array of numbers:", "s");
 HL.log(numbers, "plot");
 
 // TODO: Multi line graph
 
-// TODO: log 2d array
-//let array2d = Array.from({length: 10}, () => Array.from({length: 10}, () => Math.floor(Math.random() * 40)));
-//// TODO: call it 2d heat map
-//log(array2d, "2dnumbersarray");
+// Log 2d array as Heatmap
+let arr2d = [];
+for (let i = 0; i < 10; i++) {
+    arr2d[i] = [];
+    for (let j = 0; j < 10; j++) {
+        arr2d[i][j] = i*10 + j;
+    }
+}
+HL.log("Example of logging 2d number array as Heatmap:", "s");
+HL.log(arr2d, "heatmap");
 
 // TODO: histogram
 
@@ -30,7 +36,7 @@ let locations = [
     [37.6153, -122.3900, 'Airport'],
     [37.4422, -122.1731, 'Shopping']
 ];
-HL.log("Example of logging Lat-Long Geo locations using Google maps:", "string");
+HL.log("Example of logging Lat-Long Geo locations using Google maps:", "s");
 HL.log(locations, "GoogleMaps");
 
 // log custom object
@@ -39,7 +45,7 @@ let rectangle = {
     width : 10,
     color : "green"
 }
-HL.log("Example of logging custom object:", "string");
+HL.log("Example of logging custom object:", "s");
 // note, default logging type is "object"
 HL.log(rectangle);
 
@@ -53,7 +59,7 @@ let car = {
         },
         color : "red"
     };
-HL.log("Example of logging custom object with nested custom objects:", "string");
+HL.log("Example of logging custom object with nested custom objects:", "s");
 HL.log(car);
 
 // TODO: Example of logging with tags
